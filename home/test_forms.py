8a18ad936ac3d15e263d2e18023801a2ea4d9d5a
amfairley <adamfairley1990@gmail.com>
2024-10-21 16:18:05 +0100

    [TDD Red]: test that the checkin date is required

--- home/test_forms.py
+++ home/test_forms.py
@@ -3,6 +3,13 @@ from .forms import BookingForm
 
 
 class BookingFormTest(TestCase):
+    '''Tests for the booking form'''
     def test_booking_form_exists(self):
+        '''Tests that the booking form exists and is defined'''
         form = BookingForm()
         self.assertIsInstance(form, BookingForm)
+
+    def test_name_form_required(self):
+        '''Tests that the name form is required'''
+        form = BookingForm({'check_in_date': ''})
+        self.assertTrue(form.is_valid())
