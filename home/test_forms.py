f6c760b64bbc6123b57a9ae2771c7f038a65f816
amfairley <adamfairley1990@gmail.com>
2024-10-21 16:13:03 +0100

    [TDD Green]: Create a booking form

--- home/test_forms.py
+++ home/test_forms.py
@@ -3,6 +3,6 @@ from .forms import BookingForm
 
 
 class BookingFormTest(TestCase):
-    def booking_form_exists(self):
+    def test_booking_form_exists(self):
         form = BookingForm()
         self.assertIsInstance(form, BookingForm)
