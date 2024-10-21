83825d2c67caf6e4a20d05869315d3a13a6f9997
amfairley <adamfairley1990@gmail.com>
2024-10-21 06:26:19 +0100

    [TDD Red]: Add test for correct template used. Issue: #5

--- home/test_views.py
+++ home/test_views.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 
 
 class TestHomeView(TestCase):
+    '''A class to test the home views'''
     def test_view_works(self):
+        '''A test to check that the correct template is used and page loads'''
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
