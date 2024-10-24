c5c02d8e12eb1792450b5e873c6a07815c0c7f5f
amfairley <adamfairley1990@gmail.com>
2024-10-24 13:13:36 +0100

    build: add home context processor for booking form

--- nunisi/settings.py
+++ nunisi/settings.py
@@ -77,6 +77,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Booking form context processor
+                'home.context_processor.booking_form',
             ],
         },
     },
