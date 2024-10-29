2d215faa8d19818478378844e113fd6fa7c42134
amfairley <adamfairley1990@gmail.com>
2024-10-29 14:42:38 +0000

    admin: update columns shown in admin page for rooms models. Issue: #8

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 from .models import Room, Amenities
 
 
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sanitised_name',
+        'amenities',
+        'description',
+        'image_url',
+        'image',
+        'price',
+        'unavailability'
+    )
+
+    ordering = ('name',)
+
+
+class AmenitiesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sanitised_name',
+        'icon'
+    )
+
+    ordering = ('name',)
+
+
 # Register your models here.
-admin.site.register(Room)
-admin.site.register(Amenities)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Amenities, AmenitiesAdmin)
