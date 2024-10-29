439873d2dffbb1f13bbcb4109301a126d9052ec4
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:05:23 +0000

    [TDD Red]: test room name field max length. Issue: #8

--- rooms/test_models.py
+++ rooms/test_models.py
@@ -13,10 +13,12 @@ class RoomModelTest(TestCase):
             'Room', [model.__name__ for model in apps.get_models()]
         )
 
-    def test_name_field_type(self):
-        '''Test the name field type'''
+    def test_name_field(self):
+        '''Test the name field'''
         # Get the 'name' field from the model
         field = Room._meta.get_field('name')
         # Check that the field is a CharField
         self.assertIsInstance(field, CharField)
         self.assertEqual(field.get_internal_type(), 'CharField')
+        # Check the max length of the CharField
+        self.assertEqual(field.max_length, 254)
