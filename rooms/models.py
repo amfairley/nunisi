dd4571cb566952d590513f126664a0b1af0817a6
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:57:32 +0000

    [TDD Green]: add price field to room model. Issue: #8

--- rooms/models.py
+++ rooms/models.py
@@ -10,3 +10,9 @@ class Room(models.Model):
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
