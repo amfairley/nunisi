231be0c5035451f48fd6e3215e7a90e30d716a09
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:23:06 +0000

    [TDD Green]: Add amenities field to room model. Issue: #8

--- rooms/models.py
+++ rooms/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=254)
     sanitised_name = models.CharField(max_length=254)
+    amenities = models.JSONField(default=list)
