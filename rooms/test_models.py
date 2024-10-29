52f1804d45d3204b125bddb7cfc5f4634386d816
amfairley <adamfairley1990@gmail.com>
2024-10-29 10:58:04 +0000

    [TDD Red]: Check room model exists. Issue: #8

--- rooms/test_models.py
+++ rooms/test_models.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.apps import apps
+from .models import Room
 
-# Create your tests here.
+
+class RoomModelTest(TestCase):
+    '''Tests for the Room model'''
+    def test_room_model_exists(self):
+        '''Check that the app is installed and model defined'''
+        self.assertTrue(apps.is_installed('rooms'))
+        self.assertIn(
+            'Room', [model.__name__ for model in apps.get_models()]
+        )
