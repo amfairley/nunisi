d196ab70425fe9a41f1ad3d38929dd35a6bcb72f
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:01:38 +0000

    [TDD Red]: Test room name field type. Issue: #8

--- rooms/test_models.py
+++ rooms/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.apps import apps
+from django.db.models import CharField
 from .models import Room
 
 
@@ -11,3 +12,11 @@ class RoomModelTest(TestCase):
         self.assertIn(
             'Room', [model.__name__ for model in apps.get_models()]
         )
+
+    def test_name_field_type(self):
+        '''Test the name field type'''
+        # Get the 'name' field from the model
+        field = Room._meta.get_field('name')
+        # Check that the field is a CharField
+        self.assertIsInstance(field, CharField)
+        self.assertEqual(field.get_internal_type(), 'CharField')
