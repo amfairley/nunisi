9ea0adbe3743e98d216b8b7fedd6a53c96604166
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:28:29 +0000

    [TDD Green]: Add room model description field. Issue: #8

--- rooms/models.py
+++ rooms/models.py
@@ -5,3 +5,6 @@ class Room(models.Model):
     name = models.CharField(max_length=254)
     sanitised_name = models.CharField(max_length=254)
     amenities = models.JSONField(default=list)
+    description = models.TextField(
+        default='Room Description'
+    )
