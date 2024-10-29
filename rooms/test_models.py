62686178a46ef3c6710d61cadcef2102176cdfd3
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:22:32 +0000

    [TDD Red]: Add tests for room model amenities field. Issue: #8

--- rooms/test_models.py
+++ rooms/test_models.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from django.apps import apps
-from django.db.models import CharField
+from django.db.models import CharField, JSONField
 from .models import Room
 
 
 class RoomModelTest(TestCase):
     '''Tests for the Room model'''
+    def setUp(self):
+        '''Creates an instance of the model to test'''
+        self.instance = Room.objects.create(
+            name="Test Name",
+            sanitised_name="Sanitised Test Name",
+            amenities=[1, 2, 3]
+        )
+
     def test_room_model_exists(self):
         '''Check that the app is installed and model defined'''
         self.assertTrue(apps.is_installed('rooms'))
@@ -33,4 +41,23 @@ class RoomModelTest(TestCase):
         field = Room._meta.get_field('sanitised_name')
         # Check the field is a Charfield
         self.assertIsInstance(field, CharField)
+        # Check the max length of the CharField
+        self.assertEqual(field.max_length, 254)
+        # Check null status
+        self.assertFalse(field.null)
+        # Check blank status
+        self.assertFalse(field.blank)
+
+    def test_amenities_field(self):
+        '''Test the amenities field'''
+        # Get the 'amenities' field from the model
+        field = Room._meta.get_field('amenities')
+        # Check the field is a JSONField
+        self.assertIsInstance(field, JSONField)
+        # Check the JSONField default
+        self.assertEqual(self.instance.amenities, [1, 2, 3])
+        # Check null status
+        self.assertFalse(field.null)
+        # Check blank status
+        self.assertFalse(field.blank)
 
