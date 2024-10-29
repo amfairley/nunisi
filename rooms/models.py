b5aed26f45ad83f032ea7aa83ad80a96d16f3f41
amfairley <adamfairley1990@gmail.com>
2024-10-29 11:53:14 +0000

    [TDD Green]: add image field to room model. Issue: #8

--- rooms/models.py
+++ rooms/models.py
@@ -9,3 +9,4 @@ class Room(models.Model):
         default='Room Description'
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
