c2f3a2527167fa001634a1aefe97cd747b919795
amfairley <adamfairley1990@gmail.com>
2024-10-30 14:18:09 +0000

    [TDD Green]: add generate order number method to order model. Issue: #9

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -13,4 +14,13 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+
+    def _generate_order_number(self):
+        '''Generate a random, unique order number using UUID'''
+        return uuid.uuid4().hex.upper()
