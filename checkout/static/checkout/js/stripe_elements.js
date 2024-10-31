73b62723aa4e2cc2fca4d543906c4215255e0e3d
amfairley <adamfairley1990@gmail.com>
2024-10-31 11:04:05 +0000

    stripe: add real time validation of card errors. Issue: #9

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,11 +1,11 @@
-/* Get stripe keys */
+// Get stripe keys
 var stripe_public_key = document.getElementById('id_stripe_public_key').textContent.slice(1,-1);
 var client_secret = document.getElementById('id_client_secret').textContent.slice(1,-1);
 
-/* Set up stripe */
+// Set up stripe
 var Stripe = Stripe(stripe_public_key);
 
-/* Create instances of stripe elements */
+// Create instances of stripe elements
 var elements = Stripe.elements();
 var style = {
     base: {
@@ -24,3 +24,20 @@ var style = {
 };
 var card = elements.create('card');
 card.mount('#card-element', {style: style});
+
+// Handle real-time validation errors on the card element
+card.addEventListener('change', function(event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        errorDiv.innerHTML = `
+            <div>
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${event.error.message}</span>
+            </div>
+        `;
+    } else {
+        errorDiv.textContent = '';
+    }
+});
