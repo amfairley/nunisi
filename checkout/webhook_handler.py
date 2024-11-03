94ffc774b24f3db4cd58178a32a503ec13d868c3
amfairley <adamfairley1990@gmail.com>
2024-11-03 13:02:13 +0000

    stripe: correct billing details to shipping details. Issue: #9

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -40,15 +40,15 @@ class StripeWH_Handler:
             # Get order info from payment intent (billing details in js)
             try:
                 order = Order.objects.get(
-                    full_name__iexact=billing_details.name,
+                    full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
-                    phone_number__iexact=billing_details.phone,
-                    country__iexact=billing_details.address.country,
-                    postcode__iexact=billing_details.address.postal_code,
-                    town_or_city__iexact=billing_details.address.city,
-                    street_address1__iexact=billing_details.address.line1,
-                    street_address2__iexact=billing_details.address.line2,
-                    county__iexact=billing_details.address.state,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
                     order_total=grand_total,
                     stripe_pid=pid,
                 )
@@ -71,15 +71,15 @@ class StripeWH_Handler:
             order = None
             try:
                 order_form_data = {
-                    'full_name': billing_details.name,
+                    'full_name': shipping_details.name,
                     'email': billing_details.email,
-                    'phone_number': billing_details.phone,
-                    'country': billing_details.address.country,
-                    'postcode': billing_details.address.postal_code,
-                    'town_or_city': billing_details.address.city,
-                    'street_address1': billing_details.address.line1,
-                    'street_address2': billing_details.address.line2,
-                    'county': billing_details.address.state,
+                    'phone_number': shipping_details.phone,
+                    'country': shipping_details.address.country,
+                    'postcode': shipping_details.address.postal_code,
+                    'town_or_city': shipping_details.address.city,
+                    'street_address1': shipping_details.address.line1,
+                    'street_address2': shipping_details.address.line2,
+                    'county': shipping_details.address.state,
                     'order_total': grand_total,
                     'stripe_pid': pid,
                 }
