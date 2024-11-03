b676d6bdc69cd961d4ed32186a1fa0ffea1ab803
amfairley <adamfairley1990@gmail.com>
2024-11-03 06:40:49 +0000

    stripe: add cache checkout data to urls. Issue: #9

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-
+from .webhooks import webhook
 
 urlpatterns = [
-    path('', views.checkout, name="checkout")
+    path('checkout/', views.checkout, name='checkout'),
+    path('checkout/success/', views.checkout, name='payment'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
