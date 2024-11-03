38c9d09852fce81cdd09316962c1b1d06eb3d497
amfairley <adamfairley1990@gmail.com>
2024-11-03 14:59:48 +0000

    profile: update UserProfile model. Issue: #10

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,10 +6,13 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=150, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=True, blank=True)
+    country = models.CharField(max_length=56, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=True, blank=True)
     street_address1 = models.CharField(max_length=80, null=True, blank=True)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     newsletter = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
