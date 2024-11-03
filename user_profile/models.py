951b32e003d645d9b57311707bde027e17557cde
amfairley <adamfairley1990@gmail.com>
2024-11-03 15:43:01 +0000

    [TDD Green]: build Trip model. Issue: #10

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rooms.models import Room
 
 
 class UserProfile(models.Model):
@@ -16,3 +17,25 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Trip(models.Model):
+    profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    start_date = models.DateField(null=False, blank=False)
+    end_date = models.DateField(null=False, blank=False)
+    adults = models.IntegerField(null=False, blank=False)
+    children = models.IntegerField(default=0)
+    infants = models.IntegerField(default=0)
+    cost = models.DecimalField(null=False, max_digits=10, decimal_places=2)
+    cancelled = models.BooleanField(default=False)
