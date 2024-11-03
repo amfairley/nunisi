30c909f5b81c4f1b30ec28d9416a3fe1bcf84349
amfairley <adamfairley1990@gmail.com>
2024-11-03 14:30:09 +0000

    profile: add UserProfile model to admin. Issue: #10

---
+++ user_profile/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from . models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'user',
+    )
+
+    list_display = (
+        'user',
+        'full_name',
+        'phone_number',
+        'country',
+        'postcode',
+        'town_or_city',
+        'street_address1',
+        'street_address2',
+        'county',
+        'newsletter'
+    )
+
+    ordering = ('user',)
+
+
+# Register your models here.
+admin.site.register(UserProfile, UserProfileAdmin)
