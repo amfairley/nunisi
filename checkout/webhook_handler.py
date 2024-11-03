90527cb676670f3651ff732c9e3620901b968c8e
amfairley <adamfairley1990@gmail.com>
2024-11-03 11:21:18 +0000

    stripe: add timer and while loop to combat asynchonisity. Issue: #9

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Order
 import stripe
+import time
 
 
 class StripeWH_Handler:
@@ -34,28 +35,39 @@ class StripeWH_Handler:
                 shipping_details.address[field] = None
         # Check if order exists, if it does, it's fine, if not, make it
         order_exists = False
-        # Get order info from payment intent (billing/shipping details in js)
-        try:
-            order = Order.objects.get(
-                full_name__iexact=billing_details.name,
-                email__iexact=billing_details.email,
-                phone_number__iexact=billing_details.phone,
-                country__iexact=billing_details.address.country,
-                postcode__iexact=billing_details.address.postal_code,
-                town_or_city__iexact=billing_details.address.city,
-                street_address1__iexact=billing_details.address.line1,
-                street_address2__iexact=billing_details.address.line2,
-                county__iexact=billing_details.address.state,
-                order_total=grand_total,
-            )
+        attempt = 1
+        while attempt <= 5:
+            # Get order info from payment intent (billing details in js)
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=billing_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=billing_details.phone,
+                    country__iexact=billing_details.address.country,
+                    postcode__iexact=billing_details.address.postal_code,
+                    town_or_city__iexact=billing_details.address.city,
+                    street_address1__iexact=billing_details.address.line1,
+                    street_address2__iexact=billing_details.address.line2,
+                    county__iexact=billing_details.address.state,
+                    order_total=grand_total,
+                )
 
-            # If order exists, return 200 response
-            order_exists = True
+                # If order exists, return 200 response
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+                # Create the order if it does not exist
+        if order_exists:
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} || SUCCESS: Verified order already in database',
-                status=200)
-        except Order.DoesNotExist:
-            # Create the order if it does not exist
+                    content=(
+                        f'Webhook received: {event["type"]} || '
+                        f'SUCCESS: Verified order already in database'
+                    ), status=200
+                )
+        else:
+            order = None
             try:
                 order_form_data = {
                     'full_name': billing_details.name,
@@ -82,7 +94,10 @@ class StripeWH_Handler:
                     status=500)
         print(intent)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=(
+                f'Webhook received: {event["type"]} | '
+                f'SUCCESS: Created order in webhook'
+                ),
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
