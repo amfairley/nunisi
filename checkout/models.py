78c12f59da95947ef5e02596270216649b97d18e
amfairley <adamfairley1990@gmail.com>
2024-11-07 13:15:55 +0000

    build: add user profile foreign key to Order model. Issue: #9

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,18 @@
 import uuid
 from django.db import models
 from django_countries.fields import CountryField
+from user_profile.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+        )
     full_name = models.CharField(max_length=150, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
