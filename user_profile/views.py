f042d4c315ce2db0a0083ba1c28210e46fa8efb1
amfairley <adamfairley1990@gmail.com>
2024-11-07 13:51:39 +0000

    build: update user_profile view to go to the user's user_profile page. Issue: #10

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 # Create your views here.
 
 
 def user_profile(request):
     ''' Display the user's profile '''
+    # Get the profile from the current user
+    user_profile = get_object_or_404(UserProfile, user=request.user)
     template = 'user_profile/user_profile.html'
-    context = {}
+    context = {
+        'user_profile': user_profile,
+    }
     return render(request, template, context)
