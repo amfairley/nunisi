209a4de5fa482e1ee42d16049f2ce08cb55824c9
amfairley <adamfairley1990@gmail.com>
2024-11-07 13:45:17 +0000

    update: have profile created on sign up and updated when saving info. Issue: #10

--- user_profile/signals.py
+++ user_profile/signals.py
@@ -1,8 +1,17 @@
 from django.dispatch import receiver
-from allauth.account.signals import user_signed_up
+
 from .models import UserProfile
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
-@receiver(user_signed_up)
-def create_user_profile_on_signup(request, user, **kwargs):
-    UserProfile.objects.create(user=user)
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        # For a new user
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
