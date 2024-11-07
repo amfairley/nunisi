39fea150f051daba453a609a0a40725a99bfcfea
amfairley <adamfairley1990@gmail.com>
2024-11-07 13:23:25 +0000

    build: update country field to django countries. Issue: #10

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from rooms.models import Room
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
+    '''
+    A user profile model for default delivery information
+    and newsletter signup
+    '''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=150, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=56, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=True, blank=True)
     street_address1 = models.CharField(max_length=80, null=True, blank=True)
