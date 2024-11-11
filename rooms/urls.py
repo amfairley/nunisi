f92e19c2953c4062d7b3bf96a6e5c5f941f7ce3c
amfairley <adamfairley1990@gmail.com>
2024-11-11 14:38:06 +0000

    rooms: update urls for add and delete room pages. Issue: #8

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     path('', views.available_rooms, name="available_rooms"),
     path('rooms_superuser/', views.rooms_superuser, name="rooms_superuser"),
     path('edit_room/<int:room_id>/', views.edit_room, name="edit_room"),
-    path(
-        'delete_room/<int:room_id>/',
-        views.delete_room_confirmation,
-        name="delete_room_confirmation"
-    ),
     path('delete_room/<int:room_id>/', views.delete_room, name="delete_room"),
+    path('add_room/', views.add_room, name="add_room"),
 
 ]
