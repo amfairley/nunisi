c6d47f687c2ce322d909451e029ff8c6f7740906
amfairley <adamfairley1990@gmail.com>
2024-11-11 09:36:54 +0000

    trips: add user trip view. Issue: #10

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import UserProfile
+from .models import UserProfile, Trip
 from .forms import EditProfileForm
 
 # Create your views here.
@@ -36,3 +36,17 @@ def edit_profile(request):
         'form': form,
     }
     return render(request, 'user_profile/edit_profile.html', context)
+
+
+def trips_user(request):
+    ''' Display the user's trips '''
+    # Get the profile from the current user
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    # Get the user's trips
+    trips = Trip.objects.filter(profile=user_profile)
+
+    context = {
+        'user_profile': user_profile,
+        'trips': trips,
+    }
+    return render(request, 'user_profile/trips.html', context)
