690006ee0c4cce08e2776a5db79a512c1b917e99
amfairley <adamfairley1990@gmail.com>
2024-11-11 09:37:25 +0000

    trips: add user trip url. Issue: #10

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.user_profile, name="user_profile"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
+    path('trips/', views.trips_user, name="trips_user"),
 ]
