9335497e3c8dcec779db47c360196d21063684f4
amfairley <adamfairley1990@gmail.com>
2024-11-11 10:08:28 +0000

    trips: add url for superuser trips. Issue: #10

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.user_profile, name="user_profile"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
     path('trips/', views.trips_user, name="trips_user"),
+    path('trips_superuser/', views.trips_superuser, name="trips_superuser"),
 ]
