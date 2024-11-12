4ee05a25ea59c0570c08439fa3588f305306e276
amfairley <adamfairley1990@gmail.com>
2024-11-12 09:51:11 +0000

    bug: fix bug 3 duplicate ids

--- home/context_processor.py
+++ home/context_processor.py
@@ -4,5 +4,6 @@ from .forms import BookingForm
 def booking_form(request):
     '''Add the booking form to the context'''
     return {
-        'booking_form': BookingForm()
+        'booking_form_mobile': BookingForm(prefix="mobile"),
+        'booking_form_desktop': BookingForm(prefix="desktop"),
     }
