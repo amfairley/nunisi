3af85ea5e801de24582a0ff6dd1403787f0a0680
amfairley <adamfairley1990@gmail.com>
2024-11-13 08:26:19 +0000

    test: add testing for views. Issue: #8

--- rooms/test_views.py
+++ rooms/test_views.py
@@ -1,69 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Room, Amenities
+from datetime import date, timedelta
 
 
-class AvailableRoomsViewTest(TestCase):
+class AvailableRoomsViewTests(TestCase):
     '''Test the rooms view for available_rooms'''
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('available_rooms')
 
-    # Set up some room instances for testing
-    @classmethod
-    def setUpTestData(cls):
-        # Create amenities instances
-        cls.bed = Amenities.objects.create(name='Bed')
-        cls.double_bed = Amenities.objects.create(name='Double Bed')
-        # Create room instances
-        cls.room1 = Room.objects.create(
-            name="Room 1",
-            sanitised_name="Room One",
-            price=100,
-            amenities=[cls.bed.pk],
-            unavailability=[],
-        )
-        cls.room2 = Room.objects.create(
-            name="Room 2",
-            sanitised_name="Room Two",
-            price=150,
-            amenities=[cls.double_bed.pk],
-            unavailability=[('2024, 11, 1')],
-        )
-
-    def test_view_renders(self):
-        '''Test that the view renders correctly'''
-        response = self.client.get(reverse('available_rooms'), {
-            'check_in_date': '2024-10-15',
-            'check_out_date': '2024-10-20',
-            'adults': 1,
-            'children': 1,
-            'infants': 0,
-        })
+    def test_available_rooms_renders_correctly(self):
+        '''Test the correct template is used and works'''
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'rooms/available_rooms.html')
+
+    def test_available_rooms_context(self):
+        '''Test that the correct data is in the context'''
+        response = self.client.get(self.url)
+        self.assertIn('rooms', response.context)
+        self.assertIn('amenities', response.context)
+        self.assertIn('booking_form', response.context)
+        self.assertIn('booking_form_desktop', response.context)
+
+    def test_available_rooms_valid_data(self):
+        '''Test valid data'''
+        # Set up data
+        valid_data = {
+            'mobile-check_in_date': date.today(),
+            'mobile-check_out_date': date.today() + timedelta(days=2),
+            'mobile-adults': 2,
+        }
+        response = self.client.post(self.url, valid_data)
+        self.assertEqual(response.status_code, 200)
         self.assertIn('valid_rooms', response.context)
 
-    def test_room_unavailability(self):
-        """Test that room 2 is not shown due to unavailability"""
-        response = self.client.get(reverse('available_rooms'), {
-            'check_in_date': '2024-10-31',
-            'check_out_date': '2024-11-02',
-            'adults': 2,
-            'children': 0,
-            'infants': 1,
-        })
-        valid_rooms = response.context['valid_rooms']
-        self.assertEqual(len(valid_rooms), 0)
-
-    def test_total_cost_calculation(self):
-        """Test the total cost calculation"""
-        response = self.client.get(reverse('available_rooms'), {
-            'check_in_date': '2024-10-15',
-            'check_out_date': '2024-10-18',
-            'adults': 1,
-            'children': 0,
-            'infants': 0,
-        })
-        # Get valid_rooms from the context
-        valid_rooms = response.context['valid_rooms']
-        for room in valid_rooms:
-            total_cost = room['room'].price * response.context['total_days']
-            self.assertEqual(room['total_cost'], total_cost)
+    def test_available_room_invalid_data(self):
+        '''Test invalid data'''
+        invalid_data = {'mobile-check_in_date': 'invalid-date'}
+        response = self.client.post(self.url, invalid_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['booking_form'].is_valid())
