4aa2d40114a347a3874ed66f76e8155c3d215c16
amfairley <adamfairley1990@gmail.com>
2024-11-13 17:41:54 +0000

    checkout: update urls. Issue: #9

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,6 +5,10 @@ from .webhooks import webhook
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout/success/', views.checkout, name='payment'),
-    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path(
+        'cache_checkout_data/',
+        views.cache_checkout_data,
+        name='cache_checkout_data'
+    ),
     path('wh/', webhook, name='webhook'),
 ]
