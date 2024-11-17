ae54d719ac413460eeae847349d048bedd93977f
amfairley <adamfairley1990@gmail.com>
2024-11-17 19:14:38 +0000

    fix: add ip address to allowed hosts for local development environment

--- nunisi/settings.py
+++ nunisi/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nunisi-hotel-and-spas.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'nunisi-hotel-and-spas.herokuapp.com',
+    'localhost',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
