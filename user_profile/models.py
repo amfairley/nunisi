0ed9b733de1f3e3b5b9d9e8555c4aca81492dc5a
amfairley <adamfairley1990@gmail.com>
2024-11-17 12:58:46 +0000

    build: add email to userprofile model. Issue: #10

--- user_profile/models.py
+++ user_profile/models.py
@@ -10,6 +10,7 @@ class UserProfile(models.Model):
     and newsletter signup
     '''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, null=True, blank=True)
     full_name = models.CharField(max_length=150, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=True, blank=True)
