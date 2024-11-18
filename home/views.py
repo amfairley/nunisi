4fde128fe8951b55d66bfe2dc57132294ae0c9cd
amfairley <adamfairley1990@gmail.com>
2024-11-18 08:27:07 +0000

    style: add media link in view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
     """A view to return the home page"""
-    return render(request, 'home/index.html')
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(request, 'home/index.html', context)
