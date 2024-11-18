5b5acab9907e11a4f8b4269f137a8389f4fd758d
amfairley <adamfairley1990@gmail.com>
2024-11-18 15:07:38 +0000

    fix: update email port and TLS to SSL

--- nunisi/settings.py
+++ nunisi/settings.py
@@ -239,8 +239,9 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
+    EMAIL_PORT = 465
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
