68f2f7f18fed7ce00ad064e83f9aef1448e23e33
amfairley <adamfairley1990@gmail.com>
2024-12-03 14:35:40 +0000

    JavaScript: add docstrings to JS functions. Issue: #4

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,9 @@
-// Booking form guest number update
 function updateGuestCount() {
+    /**
+     * Gets the number of adults, children, and infants and calculates
+     * total guests, displaying it on the screen.
+     * Works for both the navigation bar search form and index page form
+     */
     // Get values from the form in real time (0 if no value) for both forms
     const adultsTopNav = parseInt(
         document.querySelector("#top-nav #id_desktop-adults").value
@@ -44,8 +48,11 @@ function updateGuestCount() {
     }
 }
 
-// Open calendar widgets when clicking entire input area
 document.addEventListener("DOMContentLoaded", function () {
+    /**
+     * Allows the user to open the calendar widgets by clicking anywhere
+     * inside the check in/out date input areas.
+     */
     // Get the checkin/out inputs for both forms
     const checkInInputTop = document.querySelector(
         "#top-nav #id_desktop-check_in_date"
