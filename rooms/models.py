b3306553c828bbe6a210d97bc1e42d2d5c7ceb59
amfairley <adamfairley1990@gmail.com>
2024-12-12 12:26:48 +0000

    Rooms: add requirements to fields for add/edit room forms. Issue: #8

--- rooms/models.py
+++ rooms/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class Room(models.Model):
-    name = models.CharField(max_length=254)
-    sanitised_name = models.CharField(max_length=254)
-    amenities = models.JSONField(default=list)
+    name = models.CharField(max_length=254, null=False, blank=False)
+    sanitised_name = models.CharField(max_length=254, null=False, blank=False)
+    amenities = models.JSONField(default=list, null=False, blank=False)
     description = models.TextField(
-        default='Room Description'
+        default='Room Description',
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(
         max_digits=6,
         decimal_places=2,
-        null=True,
-        blank=True
+        null=False,
+        blank=False
     )
     unavailability = models.JSONField(default=list, null=False, blank=True)
 
