2a1339cc1f44c4f38d8a67d9ec5e80d8953bac10
amfairley <adamfairley1990@gmail.com>
2024-12-12 13:53:28 +0000

    refactor: remove blank line

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('edit_room/<int:room_id>/', views.edit_room, name="edit_room"),
     path('delete_room/<int:room_id>/', views.delete_room, name="delete_room"),
     path('add_room/', views.add_room, name="add_room"),
-
 ]
