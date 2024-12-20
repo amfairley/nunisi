162445d89b4a6552e24524a3b6bc2a362a552d3f
amfairley <adamfairley1990@gmail.com>
2024-12-20 16:01:37 +0000

    refactor: spread paths over multiple lines. Issue: #14

--- trips/urls.py
+++ trips/urls.py
@@ -5,6 +5,20 @@ from . import views
 urlpatterns = [
     path('trips/', views.trips_user, name="trips_user"),
     path('cancel/<int:trip_id>/', views.cancel_trip, name='cancel_trip'),
-    path('cancel/<int:trip_id>/send-email/', views.send_cancellation_email, name='send_cancellation_email'),
-    path('cancel/success/', views.cancel_trip_success, name='cancel_trip_success'),
+    path(
+        'cancel/<int:trip_id>/send-email/',
+        views.send_cancellation_email,
+        name='send_cancellation_email'
+    ),
+    path('trips_superuser/', views.trips_superuser, name="trips_superuser"),
+    path(
+        'trips_superuser/toggle/<int:trip_id>/',
+        views.toggle_trip_status,
+        name='toggle_trip_status'
+    ),
+    path(
+        'cancel/success/',
+        views.cancel_trip_success,
+        name='cancel_trip_success'
+    ),
 ]
