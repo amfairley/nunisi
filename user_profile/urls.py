58713a7c432c202291c53751ade44652c9a609b1
amfairley <adamfairley1990@gmail.com>
2024-12-20 21:40:45 +0000

    User: remove trip urls from user_profile app. Issue: #10

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.user_profile, name="user_profile"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
-    path('trips/', views.trips_user, name="trips_user"),
-    path('trips_superuser/', views.trips_superuser, name="trips_superuser"),
     path('delete_user/<int:user_id>/', views.delete_user, name="delete_user"),
     path('delete_successful/',
          views.delete_successful,
