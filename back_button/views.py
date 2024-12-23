88b13295a2dad3b695afba89aa320c4b463379c0
amfairley <adamfairley1990@gmail.com>
2024-12-23 17:26:31 +0000

    Back button: troubleshoot view. Issue: #12

--- back_button/views.py
+++ back_button/views.py
@@ -7,6 +7,10 @@ def go_back_or_404(request):
     '''
     referer = request.META.get('HTTP_REFERER')
     if referer:
+        print("REFERER:")
+        print(referer)
         return HttpResponseRedirect(referer)
     else:
+        print("REFERER")
+        print("no previous page found")
         raise Http404("No previous page found.")
