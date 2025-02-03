ed8c1bd5abeb954e7ad3afff48bb7b82bb5b638a
amfairley <adamfairley1990@gmail.com>
2025-02-03 13:30:35 +0000

    stripe: update to allow emails to non logged in users. Issue: #9

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -159,9 +159,10 @@ class StripeWH_Handler:
         email = metadata.get('user_email')
 
         # Get the user if they are logged in
-        user = None
-        if email:
+        try:
             user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = None
 
         # Get the charge object
         stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
