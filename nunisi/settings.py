30616fe6f0cae37af4872d795897c1986da8b06a
amfairley <adamfairley1990@gmail.com>
2025-11-20 08:29:33 +0400

    Static: update all images to local instead of AWS

--- nunisi/settings.py
+++ nunisi/settings.py
@@ -314,3 +314,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # project-level static folder
+]
